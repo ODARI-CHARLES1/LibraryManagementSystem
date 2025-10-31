30bb77bd244a873f8a052dde961522d1b4c0d160
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-31 14:09:09 +0300

    update -insert and login functionality

--- src/services/users.Service.ts
+++ src/services/users.Service.ts
@@ -1,5 +1,10 @@
+import { match } from 'assert'
 import * as userRepository from '../repositories/user.Repository'
-import { newUser } from '../types/users.types'
+import { existingUser, newUser, User } from '../types/users.types'
+import getDate from '../Utils/generateDate.utils'
+import hashPassword from '../Utils/hashPassword.utils'
+import bcrypt from 'bcrypt'
+import jwt, { Secret } from 'jsonwebtoken'
 
 export const getAdmins=async()=>{
  const admins=await userRepository.getAdmins()
@@ -30,10 +35,55 @@ export const getUserByEmail=async(user_email:string)=>{
 }
 
 export const insertUser=async(user:newUser)=>{
-    const newUser=await userRepository.insertUser(user)
-    return newUser
+    try {
+        const existingUser=await userRepository.getUserByEmail(user.email)
+        if(!existingUser){
+            const created=await getDate()
+            const hashed= await hashPassword(user.password)
+            user.created_at=new Date(created)
+            user.password=hashed
+            const registerUser=await userRepository.insertUser(user)
+            return {registerUser}
+        }
+        else{
+            return {success:false,Message:"User already exists"}
+        }   
+    } catch (error:any) {
+        return {success:false,error:error.message}
+    }
 }
 
+export const loginUser=async(user:existingUser)=>{
+    try {
+        const existingUser=await userRepository.loginUser(user)
+        if(!existingUser){
+            return {success:false,message:"User doesn't exist, signup"}
+        }
+        const matchPassword=bcrypt.compare(user.password,existingUser[0].password)
+        if(!matchPassword){
+            return {success:false,message:"Wrong Password"}
+        }
+        const secret=process.env.JWT_SECRET  as string
+        const expires=process.env.JWT_EXPIRES as string
+        const payload={
+            id:existingUser[0].user_id,
+            username:existingUser[0].username,
+            role:existingUser[0].role,
+            created:existingUser[0].created_at,
+            updated:existingUser[0].updated_at
+        } 
+        const token=jwt.sign(payload,secret,{expiresIn:'1h'})
+
+        if(!token){
+            return {success:false,messaeg:"Try Again"}
+        }
+        const payloadWithToken={...payload,token}
+        return {success:true,message:"Logged in successfully",data:payloadWithToken}
+
+    } catch (error) {
+        throw error
+    }
+}
 export const deleteUser=async(user_id:number)=>{
    await userRepository.deleteUser(user_id)
 }
