3a8d7a9d8ad283e8bf4fffb94e1d9a485928c6f1
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-10-31 14:06:10 +0300

    update user types

--- src/types/users.types.ts
+++ src/types/users.types.ts
@@ -3,8 +3,8 @@ export interface User{
     username: string,
     email: string,
     password:string,
-    role: string,
-    created_at?: Date ,
+    role: 'Admin' | 'Member',
+    created_at?: Date,
     updated_at?: Date
 }
 
@@ -12,13 +12,32 @@ export interface newUser{
     username:string,
     email:string,
     password:string,
-    role:string,
+    role:'Admin' | 'Member',
     created_at?:Date,
 }
 
 export interface updateUser{
     username?:string,
     password?:string,
-    role?:string,
+    role?:'Admin' |'Member',
     updated_at?:Date
+}
+
+
+export interface existingUser{
+    email:string,
+    password:string
+}
+
+export interface loginJwtConfig{
+    payload:{
+        id:number
+        username:string,
+        role:string
+        created:Date
+        updated:Date
+    },
+    expires:string,
+    secret:string
+
 }
