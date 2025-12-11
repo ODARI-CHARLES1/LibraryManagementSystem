f36fe34b5a7d397bfbe109e327de5245ea18e0f8
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-12-11 22:58:17 +0300

    update origin configuration for cors configuration

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://librarymanagementsystem-7.onrender.com",
     methods:["POST","GET","PUT","DELETE"]
 }))
 
