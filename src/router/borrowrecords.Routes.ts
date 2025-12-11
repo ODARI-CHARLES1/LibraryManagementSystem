9af409e9b30ea749054d59cf03e67b8562967ade
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-12-11 10:53:24 +0300

    added authorizaton middleware for admin-user role

--- src/router/borrowrecords.Routes.ts
+++ src/router/borrowrecords.Routes.ts
@@ -4,11 +4,11 @@ import { isAuthenticated } from "../Middlewares/bearAuth";
 import { authorize } from "../Middlewares/roleAuth";
 
 const borrowRouter = Router(); 
-borrowRouter.get("/borrow-records", isAuthenticated,authorize,borrowController.getAllBorrowRecords);
+borrowRouter.get("/borrow-records", isAuthenticated,authorize("admin"),borrowController.getAllBorrowRecords);
 borrowRouter.get("/borrow-records/:borrow_id", isAuthenticated,borrowController.getBorrowRecordById);
-borrowRouter.post("/borrow-records/create", isAuthenticated,authorize,borrowController.createBorrowRecord);
-borrowRouter.put("/borrow-records/update/:borrow_id", isAuthenticated,authorize,borrowController.updateBorrowRecord);
-borrowRouter.patch("/borrow-records/clear/:borrow_id", isAuthenticated,authorize,borrowController.clearBorrowRecord);
-borrowRouter.delete("/borrow-records/delete/:borrow_id", isAuthenticated,authorize,borrowController.deleteBorrowRecord);
+borrowRouter.post("/borrow-records/create", isAuthenticated,authorize("admin"),borrowController.createBorrowRecord);
+borrowRouter.put("/borrow-records/update/:borrow_id", isAuthenticated,authorize("admin"),borrowController.updateBorrowRecord);
+borrowRouter.patch("/borrow-records/clear/:borrow_id", isAuthenticated,authorize("admin"),borrowController.clearBorrowRecord);
+borrowRouter.delete("/borrow-records/delete/:borrow_id", isAuthenticated,authorize("admin"),borrowController.deleteBorrowRecord);
 
 export default borrowRouter;
